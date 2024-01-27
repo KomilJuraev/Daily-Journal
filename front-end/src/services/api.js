3703279f98893321f5aca2b23c4dafcc31837cef
KomilJuraev <K.juraev1994@gmail.com>
2024-01-27 14:40:18 -0500

    hidding .env files

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -3,7 +3,7 @@ let BASE_URL = "";
 if (process.env.NODE_ENV === "development") {
     BASE_URL = "http://localhost:4000";
   } else if (process.env.NODE_ENV === "production") {
-    BASE_URL = "https://mern-daily-journal-backend.onrender.com";
+    BASE_URL = process.env.REACT_APP_BASE_URL;
   }
   
 //Gets all the data from database
